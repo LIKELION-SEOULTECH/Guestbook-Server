cd3d827eafc2a67326891e69cca0ad5b910ed2b4
안정후 <austinan@naver.com>
2025-03-23 00:29:26 +0900

    fix: init.sql 연결

---
+++ init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS guestbook;
+
+USE guestbook;
+
+CREATE TABLE IF NOT EXISTS posts (
+                                     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                     user_name VARCHAR(255),
+                                     password VARCHAR(255),
+                                     content TEXT,
+                                     emotion VARCHAR(50),
+                                     created_at DATETIME,
+                                     updated_at DATETIME
+);
