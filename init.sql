cbf6474c9f8d29f0b7f4d71dbd3a6a05f2c3b447
안정후 <austinan@naver.com>
2025-03-27 12:33:15 +0900

    fix: init.sql emotion type

--- init.sql
+++ init.sql
@@ -1,15 +1,15 @@
 CREATE DATABASE IF NOT EXISTS guestbook;
-
 USE guestbook;
 
-CREATE TABLE IF NOT EXISTS posts (
-                                     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                     user_name VARCHAR(255),
-                                     password VARCHAR(255),
-                                     content TEXT,
-                                     emotion VARCHAR(50),
-                                     created_at DATETIME,
-                                     updated_at DATETIME
-);
+DROP TABLE IF EXISTS posts;
 
-ALTER TABLE posts MODIFY id BIGINT AUTO_INCREMENT;
+CREATE TABLE posts
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_name  VARCHAR(255),
+    password   VARCHAR(255),
+    content    TEXT,
+    emotion    ENUM ('HORROR', 'SURPRISE', 'ANGER', 'SORROW', 'NEUTRALITY', 'HAPPINESS', 'AVERSION'),
+    created_at DATETIME,
+    updated_at DATETIME
+);
