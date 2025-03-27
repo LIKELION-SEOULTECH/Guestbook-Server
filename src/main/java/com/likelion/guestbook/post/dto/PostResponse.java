10245adddf23751faae9645a85d3daf887926907
saeyeonn <sy01lim@naver.com>
2025-03-27 13:48:49 +0900

    :adhesive_bandage: Fix: Enumerated 방식을 String으로 저장하도록 변경

--- src/main/java/com/likelion/guestbook/post/dto/PostResponse.java
+++ src/main/java/com/likelion/guestbook/post/dto/PostResponse.java
@@ -11,7 +11,7 @@ public record PostResponse(
         Long id,
         String userName,
         String content,
-        Emotion emotion,
+        String emotion,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
 ) {
